aefbb5c55a6785736a2b88497aa4b13c2b4ed8e3
TICO-79009 <takashi.matsuda@mail.toyota-shokki.co.jp>
2024-01-25 04:10:37 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # node-red-iotedge
+
+## 概要
+Azure IoT Edge Node-RED Moduleをベースに一部カスタムを行ったNodeRedのカスタムノードです。
+
+## Quick Start
+
+## Feedback
+お気づきの点があれば、ぜひIssueにてお知らせください。
+
+## LICENSE
+This project is licensed under the MIT License, see the LICENSE file for details.
---
+++ node-red-contrib-azure-iot-edge-module/myErrorFilter.js
@@ -0,0 +1,36 @@
+
+'use strict';
+
+class myErrorFilter {
+    constructor() {
+        this.Error = true;
+        this.ArgumentError = true;
+        this.ArgumentOutOfRangeError = true;
+        this.DeviceMaximumQueueDepthExceededError = true;
+        this.DeviceNotFoundError = true;
+        this.FormatError = true;
+        this.UnauthorizedError = true;
+        this.NotImplementedError = true;
+        this.NotConnectedError = true;
+        this.IotHubQuotaExceededError = true;
+        this.MessageTooLargeError = true;
+        this.InternalServerError = true;
+        this.ServiceUnavailableError = true;
+        this.IotHubNotFoundError = true;
+        this.IoTHubSuspendedError = true;
+        this.JobNotFoundError = true;
+        this.TooManyDevicesError = true;
+        this.ThrottlingError = true;
+        this.DeviceAlreadyExistsError = true;
+        this.DeviceMessageLockLostError = true;
+        this.InvalidEtagError = true;
+        this.InvalidOperationError = true;
+        this.PreconditionFailedError = true;
+        this.TimeoutError = true;
+        this.BadDeviceResponseError = true;
+        this.GatewayTimeoutError = true;
+        this.DeviceTimeoutError = true;
+        this.TwinRequestError = true;
+    }
+}
+exports.myErrorFilter = myErrorFilter;
